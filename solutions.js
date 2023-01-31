38c3e83996cd37102b4a2fe2485796ea3247f469
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-01-31 11:22:12 -0600

    1122-q3-complete

--- solutions.js
+++ solutions.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//q1
 function isBoolean(x) {
     if(typeof x === "boolean") {
         return true;
@@ -7,10 +8,61 @@ function isBoolean(x) {
         return false;
     }
 };
+// console.log(isBoolean(42)); // outputs false
+// console.log(isBoolean("true")); // outputs false
+// console.log(isBoolean(false)); // outputs true
 
-console.log(isBoolean(42)); // outputs false
-console.log(isBoolean("true")); // outputs false
-console.log(isBoolean(false)); // outputs true
+//q2
+function hasBoolean(x) {
+    for (let i = 0; i < x.length; i++) {
+        console.log(x[i]);
+        if (typeof x[i] === "boolean") {
+            return true;
+        } else {
+            return false;
+        }
+        console.log(typeof [i]);
+    }
+};
+console.log(hasBoolean([1, 2, 3, 4])); // outputs false
+// console.log(hasBoolean([1, "false", 3, 4])); // outputs false
+// console.log(hasBoolean([1, 2, true, 4])); // outputs true
+
+// q3
+const people = [ {
+    name: "Bob",
+    age: 42
+}, {
+    name: "Sue",
+    age: 27
+}, {
+    name: "Lou",
+    age: 33
+}, {
+    name: "Zanzibar",
+    age: 18
+}, {
+    name: "Gladys",
+    age: 39
+}
+];
+
+function minAge(people) {
+    let lowestAge = people[0].age;
+    for (const person of people) {
+        if (lowestAge < person.age) {
+            // lowestAge =
+            // console.log(lowestAge);
+        } else {
+            lowestAge = person.age;
+            // console.log(person.age);
+        }
+    }
+    return lowestAge;
+}
+// minAge(people);
+console.log(minAge(people)); // outputs 18
+
 
 /**
  * Write your solutions here
